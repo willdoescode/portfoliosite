a356c697822a9dce9272a1b4a41328c2b662ddc5
Will Lane <williamlane923@gmail.com>
2020-09-01 16:57:56 -0700

    made navbar streets ahead

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,35 +1,31 @@
 <template>
   <div>
-    <link rel="stylesheet" href="Regular-CSS.css">
-<!--    add regular css styling instead of bootstrap-->
-    <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
-<!--      navbar section-->
-      <a class="navbar-brand" href="/">
-<!--        coding icon to the right of the navbar goes to the root of the website-->
-        <img class="stupid" src="../assets/ah.png" width="30" height="30" alt="">
-      </a>
-      <router-link class="navbar-brand" href="/" style="color: white; font-size: 2.5vh; margin-bottom: 3px" to="/">Will does tech</router-link>
-      <li class="nav-item active">
-        <router-link class="nav-link" to="/about">About</router-link>
-      </li>
-<!--      site name also goes to root of site-->
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-<!--          list of images to include in navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a href="#" class="navbar-brand">Will Does Tech</a>
+      <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="/"><img style="width: 22px" src="../assets/home.png">&nbsp;<span class="sr-only">(current)</span></a>
-<!--            home button for navbar-->
+            <router-link class="nav-link" to="/">
+              Home
+              <span class="sr-only">(current)</span>
+            </router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" target="_blank" :href="links['Contact Me']"><img src="https://user-images.githubusercontent.com/25087769/87174308-a4680f00-c2df-11ea-90b0-5fa1fa76d2f1.png"/></a>
-<!--            email icon in navbar that goes to a mailto link-->
+            <router-link class="nav-link" to="/about">About</router-link>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" target="_blank" :href="links['My Github']"><img style="width: 22px" src="../assets/githublogo.png"/></a>
-<!--            github logo that goes to my github account-->
-          </li>
-
         </ul>
+        <span class="navbar-text">@willdoescode</span>
       </div>
     </nav>
 
@@ -69,58 +65,6 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@600&display=swap');
 /*import a nice font from google fonts*/
 
-/*all the rest of the css is just to disable the default navbar collapse button as it was not working for me*/
 
-.navbar-collapse.collapse {
-  display: block!important;
-}
-
-.navbar-nav>li, .navbar-nav {
-  float: left !important;
-}
-
-.navbar-nav.navbar-right:last-child {
-  margin-right: -15px !important;
-}
-
-.navbar-right {
-  float: right!important;
-}
-
-.navbar-brand {
-  padding-top: 10px!important;
-  transition: 0.3s;
-}
-
-.navbar-brand:hover {
-  text-decoration: underline;
-  transform: scale(1.1);
-}
-
-.nav-link {
-  transition: 0.3s;
-}
-
-.nav-link:hover {
-  transform: scale(1.1);
-}
-
-/*couldnt think of a better class name*/
-.stupid {
-  transition: 0.3s;
-}
-
-.stupid:hover {
-  transform: scale(1.1);
-}
-
-.nav-link {
-  color: coral;
-  font-size: 2.5vh;
-}
-
-.nav-link:hover {
-  text-decoration: underline;
-}
 
 </style>
