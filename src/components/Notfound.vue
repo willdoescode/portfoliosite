15184539e8f6cb555dfb4c30ff99cffd15b4caa6
Will Lane <williamlane923@gmail.com>
2020-09-01 17:05:15 -0700

    added a not found component

---
+++ src/components/Notfound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>https://www.willdoes.tech/#/{{ $route.params.id }} is not a valid url</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Notfound'
+}
+</script>
+
+<style scoped>
+h2 {
+  color: white;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from "@/components/HomePage";
 import About from "@/components/About";
+import Notfound from "@/components/Notfound";
 
 Vue.use(Router)
 
 export default new Router({
     routes: [
         { path: '/', component: HomePage },
-        { path: '/about', component: About}
+        { path: '/about', component: About},
+        { path: '/:id', component: Notfound }
     ]
 })
