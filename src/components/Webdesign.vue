960b79c37da4813c99f542fc3c849c4cd7a3bafe
Will Lane <williamlane923@gmail.com>
2020-09-01 22:24:17 -0700

    changed a margin

--- src/components/Webdesign.vue
+++ src/components/Webdesign.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <h1>This part of my website is for web design class</h1>
-    <div class="container">
-      <router-link class="nav-link" to="/webdesign/testdesign">Test Design</router-link>
-      <router-link class="nav-link" to="/webdesign/testdesign">Test Design</router-link>
-      <router-link class="nav-link" to="/webdesign/testdesign">Test Design</router-link>
-      <router-link class="nav-link" to="/webdesign/testdesign">Test Design</router-link>
+    <div class="wow">
+      <h1>This part of my website is for web design class</h1>
+      <div class="container">
+        <router-link class="nav-link" to="/webdesign/testdesign">Test Design</router-link>
+        <router-link class="nav-link" to="/webdesign/testdesign">Test Design</router-link>
+        <router-link class="nav-link" to="/webdesign/testdesign">Test Design</router-link>
+        <router-link class="nav-link" to="/webdesign/testdesign">Test Design</router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -33,5 +35,9 @@ h1 {
 .nav-link:hover {
   color: white;
   text-decoration: underline;
+}
+
+.wow {
+  margin-top: 50px;
 }
 </style>
