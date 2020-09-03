b47fca648c585fc7d04f2451da1e30f6df38c7b7
Will Lane <williamlane923@gmail.com>
2020-09-03 16:31:56 -0700

    added contacts page

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-
+  <div class="contain">
+    <div class="contacts">
+      <a href="https://github.com/willdoescode">My Github</a>
+      <a href="mailto:williamlane923@gmail.com">Email Me</a>
+    </div>
   </div>
 </template>
 
@@ -11,5 +14,40 @@ export default {
 </script>
 
 <style scoped>
+.contain {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-width: 500px;
+  margin: 10% auto;
+  text-align: center;
+}
 
+.contacts {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 30px;
+}
+
+a {
+  border: 3px solid #09FBD3;
+  padding: 10px 15px;
+  transition: 0.3s;
+  max-width: 60vw;
+  justify-content: center;
+  position: relative;
+  width: 200px;
+  margin-bottom: 2px;
+  flex-grow: 1;
+  color: darkgrey;
+  text-transform: uppercase;
+  font-weight: 800;
+}
+
+a:hover {
+  border: 3px solid #FE53BB;
+  color: white;
+}
 </style>
